a1094fe059805ad84f0a094d20bdc24b13b9be26
Kevin Scott <kevin@hitheory.com>
2018-05-30 12:51:03 -0400

    Add controls and some packages

---
+++ src/Controls/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+// import faCoffee from '@fortawesome/fontawesome-free-solid/faCoffee';
+import styles from "./styles.scss";
+
+console.log(styles);
+
+class Controls extends Component {
+  render() {
+    return (
+      <div className="controls">controls</div>
+    );
+  }
+}
+
+export default Controls;
